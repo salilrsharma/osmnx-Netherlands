7a86bfa71fdf7b349f54bb97cd829baa5c2e01cd
salilrsharma <salil31@gmail.com>
2018-12-03 18:27:10 +0100

    Create network.py

---
+++ network.py
@@ -0,0 +1,7 @@
+#import libraries
+import osmnx as ox
+import networkx as nx
+
+#call overpass api to retrieve the motorway network of the Netherlands and store in a graph G
+place_name=['South Holland, Netherlands',{'state':'Utrecht', 'country':'Netherlands'},'North Brabant, Netherlands', 'Limburg, Netherlands','Gelderland, Netherlands', 'Overijssel, Netherlands','Drenthe, Netherlands', 'Groningen, Netherlands', 'Friesland, Netherlands', 'Flevoland, Netherlands', 'North Holland, Netherlands']
+G = ox.graph_from_place(place_name, network_type='none', infrastructure='way["highway"~"motorway|motorway_links"]')
